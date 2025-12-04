e9df75a0a56dc40cf37e3718e1725ecd15c16300
surajhosmani <surajhosmani83@gmail.com>
2025-12-04 16:57:37 +0530

    service.py

--- service.py
+++ service.py
@@ -1,14 +1,15 @@
 import sys
 
+# There must be 4 arguments in total (including program name)
 if len(sys.argv) == 4:
     service_date = sys.argv[1]
     service_amount = sys.argv[2]
     service_center = sys.argv[3]
 
     print("-- CAR SERVICE INVOICE --")
-    print(f"Service Date     : {service_date}")
-    print(f"Service Amount   : ₹{service_amount}")
-    print(f"Service Centre   : {service_center}")
+    print(f"Service Date   : {service_date}")
+    print(f"Service Amount : ₹{service_amount}")
+    print(f"Service Centre : {service_center}")
     print("---- Thank You ----")
 
 else:
