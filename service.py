362af79ea24323ab9fa91d7f9be43bc2eac6686b
surajhosmani <surajhosmani83@gmail.com>
2025-12-04 17:13:52 +0530

    service.py

--- service.py
+++ service.py
@@ -1,17 +1,18 @@
 import sys
 
-# There must be 4 arguments in total (including program name)
 if len(sys.argv) == 4:
     service_date = sys.argv[1]
     service_amount = sys.argv[2]
     service_center = sys.argv[3]
 
-    print("-- CAR SERVICE INVOICE --")
-    print(f"Service Date   : {service_date}")
-    print(f"Service Amount : {service_amount}")
-    print(f"Service Centre : {service_center}")
-    print("---- Thank You ----")
-
 else:
-    print("Error: Missing Arguments!")
-    print("Usage: python invoice.py <service_date> <service_amount> <service_center>")
+    service_date = "04-12-2025"
+    service_amount = "2000"
+    service_center = "Hubli"
+
+print("---- CAR SERVICE INVOICE ----")
+print(f"Service Date     : {service_date}")
+print(f"Service Amount   : ₹{service_amount}")
+print(f"Service Centre   : {service_center}")
+print("-----------------------------------------")
+print("Thank you for servicing your car with us!")
