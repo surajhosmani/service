ec9dac3119e7407088fea481fb8333416728b673
surajhosmani <surajhosmani83@gmail.com>
2025-12-04 16:35:50 +0530

    save

---
+++ service.py
@@ -0,0 +1,16 @@
+import sys
+if len(sys.argv) == 4:
+    service_date = sys.argv[1]
+    service_amount = sys.argv[2]
+    service_center = sys.argv[3]
+
+    print("-- CAR SERVICE INVOCE --")
+    print(f"Service Date     : {service_date}")
+    print(f"Service Amount   : ₹{service_amount}")
+    print(f"Service Centre   : {service_center}")
+    print("-----------------------------------------")
+    print("Thank you for servicing your car with us!")
+
+else:
+    print("Error: Missing Arguments!")
+    print("Usage: python invoice.py <service_date> <service_amount> <service_center>")
